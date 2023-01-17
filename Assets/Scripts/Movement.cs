c2e6b5d799e30c1cb9da90902e2c5a9b6e2c45ad
Alejandro Garcia <alu0101312101@ull.edu.es>
2023-01-17 20:33:38 +0000

    que no funciona la puta acamara

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,8 +14,15 @@ public class Movement : MonoBehaviour
     }
 
     void Update() {
-        movement = (Input.GetAxis("Horizontal") * Camera.main.transform.right +
-                     Input.GetAxis("Vertical") * Camera.main.transform.forward).normalized;
+        Transform parent = Camera.main.transform.parent;
+        Camera.main.transform.parent = null;
+        float x = transform.rotation.x;
+        float z = transform.rotation.z;
+        float w = transform.rotation.w;
+        transform.rotation = new Quaternion(x,Camera.main.transform.rotation.y,z,w);
+        movement = (Input.GetAxis("Horizontal") * transform.right +
+                     Input.GetAxis("Vertical") * transform.forward).normalized;
+        Camera.main.transform.parent = parent;
     }
 
     void FixedUpdate()
