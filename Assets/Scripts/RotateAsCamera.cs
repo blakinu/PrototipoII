a58f0a266da9d11052b050c68d4ebc68ad6bc7ca
Alejandro Garcia <alu0101312101@ull.edu.es>
2023-01-17 19:51:53 +0000

    Change camara

--- Assets/Scripts/RotateAsCamera.cs
+++ Assets/Scripts/RotateAsCamera.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RotateAsCamera : MonoBehaviour
 {
-    public GameObject _maincam;
+    private GameObject _maincam;
+    // void Start() {
+    //     device = InputSystem.GetDevice<XRNodeState>().device;
+    // }
+
     // Update is called once per frame
     void Update()
     {
+        Transform parent = _maincam.transform.parent;
         _maincam.transform.parent = null;
         float x = transform.rotation.x;
         float z = transform.rotation.z;
         float w = transform.rotation.w;
-        transform.rotation.Set(x,_maincam.transform.rotation.y,z,w);
-        _maincam.transform.parent = transform;
+                // Vector3 rotation = InputDevice.
+                //  InputTracking.GetLocalRotation(XRNode.Head).eulerAngles;
+        transform.rotation.Set(x,Camera.main.transform.rotation.y,z,w);
+        _maincam.transform.parent = parent;
     }
 }
