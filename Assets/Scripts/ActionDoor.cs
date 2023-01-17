dac772a4e6345ab5dec0dfece35c41f406a53cd0
Jaime <alu0101329093@ull.edu.es>
2023-01-17 20:11:09 +0000

    feat: Add door action

---
+++ Assets/Scripts/ActionDoor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionDoor : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetAxisRaw("Action") == 1)
+        {
+            animator.SetTrigger("action");
+        }
+    }
+}
