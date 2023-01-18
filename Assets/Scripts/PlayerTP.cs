17328a4ae5f4c33f0f0fffea4d12dc809d1b753c
Jaime <alu0101329093@ull.edu.es>
2023-01-18 21:39:20 +0000

    feat: Add tp transition

--- Assets/Scripts/PlayerTP.cs
+++ Assets/Scripts/PlayerTP.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class PlayerTP : MonoBehaviour
 {
     public PlatformTP[] platforms;
+    public Animator animator;
     private CharacterController characterController;
+    private GameObject platformToTP;
 
-    void Start() {
+    void Start()
+    {
         characterController = transform.GetComponent<CharacterController>();
         foreach (PlatformTP plat in platforms)
             plat.OnPlayerPress += TPto;
+        platformToTP = null;
     }
-    void TPto(GameObject platformToTP) {
+
+    void Update()
+    {
+        if (platformToTP == null || !animator.GetCurrentAnimatorStateInfo(0).IsName("Wait")) return;
+
         characterController.enabled = false;
         transform.position = platformToTP.gameObject.transform.position +
-                Vector3.Scale(platformToTP.GetComponent<Renderer>().bounds.size, new Vector3(0,1,0));
+                Vector3.Scale(platformToTP.GetComponent<Renderer>().bounds.size, new Vector3(0, 1, 0));
         characterController.enabled = true;
+        platformToTP = null;
+        animator.SetBool("TP", false);
+    }
+
+    void TPto(GameObject platform)
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("AfterTP") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Idle")) return;
+
+        platformToTP = platform;
+        animator.SetBool("TP", true);
     }
 }
