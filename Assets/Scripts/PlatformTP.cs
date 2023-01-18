5d8a09ab0867eacfbff6c95b3b265c1d1706f4c7
Alejandro Garcia <alu0101312101@ull.edu.es>
2023-01-18 18:57:10 +0000

    2do piso

--- Assets/Scripts/PlatformTP.cs
+++ Assets/Scripts/PlatformTP.cs
@@ -9,19 +9,28 @@ public class PlatformTP : MonoBehaviour
     public event PlatformTPing OnPlayerPress;
     
     static private bool key = false;
+    static private bool inside = false;
 
     void Update() {
-        if (Input.GetKeyDown("joystick button 2")) {
+        if (Input.GetKeyDown("joystick button 0") && inside) {
             key = true;
         }
     }
 
     void OnTriggerStay(Collider other) {
-        Debug.Log("TRIGGER");
-        if (other.gameObject.tag == "Player" && key) {
-            Debug.Log("PRESSED");
-            key = false;
-            OnPlayerPress(PlatformToTP);
+        if (other.gameObject.tag == "Player") {
+            inside = true;
+            if (key) {
+                Debug.Log("PRESSED");
+                key = false;
+                OnPlayerPress(PlatformToTP);
+            }
         }
     }
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            inside = false;
+        }
+    }
+
 }
--- Assets/Scripts/PlayerTP.cs
+++ Assets/Scripts/PlayerTP.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerTP : MonoBehaviour
 {
     public PlatformTP[] platforms;
+    private CharacterController characterController;
 
     void Start() {
+        characterController = transform.GetComponent<CharacterController>();
         foreach (PlatformTP plat in platforms)
             plat.OnPlayerPress += TPto;
     }
     void TPto(GameObject platformToTP) {
+        characterController.enabled = false;
         transform.position = platformToTP.gameObject.transform.position +
                 Vector3.Scale(platformToTP.GetComponent<Renderer>().bounds.size, new Vector3(0,1,0));
+        characterController.enabled = true;
     }
 }
